f1b9032745985646376c0aee107d0314d5535977
SAISH ANAND KOTIAN <saishkotian213@gmail.com>
2026-06-28 18:25:13 +0530

    teacher authentication

--- src/components/header.py
+++ src/components/header.py
@@ -6,11 +6,21 @@ def get_base64(image_path):
         return base64.b64encode(img.read()).decode()
 
 def header_home():
-    logo = get_base64("Logo.png")
+    logo = get_base64("Logo1.png")
 
     st.markdown(f"""
         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px">
-            <img src="data:image/png;base64,{logo}" style="height:100px;" alt="MONAE Logo">
-            <h1 style='text-align:center; color:#E0E3FF'>SNAP<br/>CLASS</h1>
+            <img src="data:image/png;base64,{logo}" style="height:100px; border-radius:25%" alt="MONAE Logo">
+            <h1 style='text-align:center; color:#E0E3FF'>MONAE</h1>
+        </div>
+                """, unsafe_allow_html=True)
+
+def header_dashboard():
+    logo = get_base64("Logo1.png")
+
+    st.markdown(f"""
+        <div style="display:flex; align-items:center; justify-content:center; gap:10px">
+            <img src="data:image/png;base64,{logo}" style="height:85px; border-radius:25%" alt="MONAE Logo">
+            <h2 style='text-align:left; color:#5865F2'>MONAE</h1>
         </div>
                 """, unsafe_allow_html=True)
